253c84857c8419047481bb75795a3d9bee7da984
kanade9 <maronkazu94@gmail.com>
2020-05-27 21:54:58 +0900

    [WIP] add model.py

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import urllib
+import zipfile
+import urllib.request
 
 url = "http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip"
 save_path = "./data/captions_train-val2014.zip"
@@ -8,9 +9,8 @@ save_path = "./data/captions_train-val2014.zip"
 if not os.path.exists(save_path):
     urllib.request.urlretrieve(url, save_path)
 
-with zipfile.ZipFile('.data/captions_train-val2014') as zip_file:
-    zip_file.extractall('.data/')
-
+with zipfile.ZipFile(save_path) as zip_file:
+    zip_file.extractall('./data/')
 
 url = "http://images.cocodataset.org/zips/train2014.zip"
 save_path = "./data/train2014.zip"
@@ -18,9 +18,8 @@ save_path = "./data/train2014.zip"
 if not os.path.exists(save_path):
     urllib.request.urlretrieve(url, save_path)
 
-with zipfile.ZipFile('./data/train2014.zip') as zip_file:
-    zip_file.extractall('.data/')
-
+with zipfile.ZipFile(save_path) as zip_file:
+    zip_file.extractall('./data/')
 
 url = "http://images.cocodataset.org/zips/val2014.zip"
 save_path = "./data/val2014.zip"
@@ -28,5 +27,5 @@ save_path = "./data/val2014.zip"
 if not os.path.exists(save_path):
     urllib.request.urlretrieve(url, save_path)
 
-with zipfile.ZipFile('./data/val2014.zip') as zip_file:
-    zip_file.extractall('.data/')
+with zipfile.ZipFile(save_path) as zip_file:
+    zip_file.extractall('./data/')
---
+++ model.py
@@ -0,0 +1,65 @@
+import torch
+import hydra
+import torch.nn as nn
+import torchvision.models as models
+from torch.nn.utils.rnn import pack_padded_sequence
+
+
+class EncoderCNN(nn.Module):
+    @hydra.main(config_path="config/config.yaml")
+    def __init__(self, embed_size):
+
+    # pretrainされたresnet512のモデルを読み込み、fc層に送る
+    super(EncoderCNN, self).__init__()
+    resnet = models.resnet152(pretrained=True)
+    # 最後のfc層を削除する
+    modules = list(resnet.children())[:-1]
+    self.resnet = nn.Sequential(*modules)
+    self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+    self.bn = nn.BatchNorm1d(embed_size, momentum=cfg.train.momentum)
+
+    def forward(self, images):
+        # 入力された画像から特徴ベクトルを抽出する
+        with torch.no_grad():
+            features = self.resnet(images)
+        features = features.reshape(features.size(0), -1)
+        features - self.bn(self.linear(features))
+
+        return features
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, num_layers, max_seq_length=20):
+
+    # ハイパラをセットして層を組み立てる
+    super(DecoderRNN, self).__init__()
+    self.embed = nn.Embedding(vocab_size, embed_size)
+    self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+    self.linear = nn.Linear(hidden_size, vocab_size)
+    self.max_seg_length = max_seq_length
+
+    def forward(self, features, captions, length):
+        # 画像のベクトル特徴量をデコードしてキャプションを生成する
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
+        hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
+        return outputs
+
+    def sample(self, features, states=None):
+        # 貪欲法を使って与えられた画像からキャプションを生成する(beam searchを使うと改善するってどこかの記事に書いてあった)
+        # 別のアルゴリズムでも検討してみたい。
+
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(self.max_seg_length):
+            hiddens, sates = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        sampled_ids = torch.stack(sampled_ids, 1)
+        return sampled_ids
+
